c6da8732a636638a1fdfd9bae5d98dfbfe69cf2d
Arfaoui Selma <selma.arfaoui@etu.univ-amu.fr>
2020-03-20 09:11:16 +0100

    Archive: Modification de addArticles() pour choisir le fichier atom

--- src/main/java/app/arxivorg/model/Archive.java
+++ src/main/java/app/arxivorg/model/Archive.java
@@ -30,34 +30,21 @@ public class Archive {
         return articles;
     }
 
-    public void addArticles(final String[] args) {
 
-        //Récupération d'une instance de la classe "DocumentBuilderFactory"
+    public void addArticles(File file) {
+
         final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
         try {
 
-             //Création d'un parseur
             final DocumentBuilder builder = factory.newDocumentBuilder();
-
-             //Création d'un Document
-            final Document document= builder.parse(new File("test.xml"));
-
-             //Récupération de l'Element racine
+            final Document document= builder.parse(file);
             final Element racine = document.getDocumentElement();
-
-            //Affichage de l'élément racine
-            System.out.println("\n*************RACINE************");
-            System.out.println(racine.getNodeName());
-
-             //Récupération des articles
             final NodeList entry = racine.getElementsByTagName("entry");
             final int nbRacineNoeuds = entry.getLength();
 
 
             for (int i = 0; i < nbRacineNoeuds; i++) {
-                System.out.println("\n*************ARTICLE " + (i+1) + "************\n");
-
                 if(entry.item(i).getNodeType() == Node.ELEMENT_NODE) {
                     final Element article = (Element) entry.item(i);
 
@@ -68,43 +55,27 @@ public class Archive {
                     final String published = article.getElementsByTagName("published").item(0).getTextContent();
                     final String summary = article.getElementsByTagName("summary").item(0).getTextContent();
 
-
-                    //Affichage des id, mises à jour, dates de publication, titres, résumés
-                    System.out.println("id : " +  id);
-                    System.out.println("updated : " +  updated);
-                    System.out.println("published : " +  published + "\n");
-                    System.out.println("title : " +  title + "\n");
-                    System.out.println("summary : " +  summary);
-                    System.out.println("auteur : ");
-
-                    List<String> authorslist = new ArrayList<>();
-
                     //Récupérations de tous les auteurs pour chaque article
+                    List<String> authorslist = new ArrayList<>();
                     for(int j = 0; j < article.getElementsByTagName("author").getLength(); j++){
                         final String author = article.getElementsByTagName("author").item(j).getTextContent();
                         authorslist.add(author);
-                        //Affichage des auteurs
-                        System.out.println(author);
                     }
 
+                    //Récupération des liens pdf et arxiv
                     String pdf = "";
                     String arxiv = "";
-                    //Récupération des liens pdf et arxiv
                     for(int k = 0; k < article.getElementsByTagName("link").getLength(); k++){
                         final String links = article.getElementsByTagName("link").item(k).getAttributes().item(0).getTextContent();
                         arxiv = article.getElementsByTagName("link").item(0).getAttributes().item(0).getTextContent();
                         pdf = article.getElementsByTagName("link").item(1).getAttributes().item(0).getTextContent();
-                        //Affichage des liens
-                        System.out.println("link : " + links);
                     }
 
-                    List<String> categorylist = new ArrayList<>();
                     //Récupération des catégories
+                    List<String> categorylist = new ArrayList<>();
                     for (int l = 0; l < article.getElementsByTagName("category").getLength(); l++){
                         final String category = article.getElementsByTagName("category").item(l).getAttributes().item(0).getTextContent();
                         categorylist.add(category);
-                        //Affichage des catégories
-                        System.out.println("category : " + category);
                     }
 
                     Authors authors = new Authors(authorslist);
