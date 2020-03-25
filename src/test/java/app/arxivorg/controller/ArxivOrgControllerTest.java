89dcd692f0828a15cd284741d61e23633bb86532
Arfaoui Selma <selma.arfaoui@etu.univ-amu.fr>
2020-03-25 10:34:59 +0100

    ArxivOrgControllerTest: méthode test pour displayArticles()

--- src/test/java/app/arxivorg/controller/ArxivOrgControllerTest.java
+++ src/test/java/app/arxivorg/controller/ArxivOrgControllerTest.java
@@ -3,8 +3,12 @@ package app.arxivorg.controller;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.assertions.api.Assertions;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
@@ -21,4 +25,11 @@ class ArxivOrgControllerTest {
         stage.show();
     }
 
+    @Test
+    public void displayArticlesTest(FxRobot robot){
+        ListView<String> listView = robot.lookup("#listView").queryListView();
+        Assertions.assertThat(listView).isNotEmpty();
+        Assertions.assertThat(listView).hasExactlyNumItems(1000);
+        Assertions.assertThat(listView).isVisible();
+    }
 }
