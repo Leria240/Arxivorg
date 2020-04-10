31c975456ba059b412644d4c0a2678f662cf9e66
asus <khalilkouki14@gmail.com>
2020-04-10 16:49:15 +0200

    Archive :implémentation de la méthode qui télécharge un fichier

--- src/main/java/app/arxivorg/model/Article.java
+++ src/main/java/app/arxivorg/model/Article.java
@@ -1,21 +1,14 @@
 package app.arxivorg.model;
 
 
-import org.apache.commons.io.IOUtils;
-
-import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.List;
-
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
 
 
 public class Article {
@@ -112,97 +105,21 @@ public class Article {
         favoriteItem = !favoriteItem;
     }
 
-    public void download() {
-        URL url = this.getURL_PDF();
-        try (InputStream in = url.openStream()) {
-            Files.copy(in, Paths.get("../../someArticle.pdf"), StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
-    private final int BUFFER_SIZE = 4096;
-
-    /**
-     * Downloads a file from a URL
-     *
-     * @param saveDir path of the directory to save the file
-     * @throws IOException
-     */
-    public void downloadFile(String saveDir)
-            throws IOException {
-        URL url = this.getURL_PDF();
-        HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
-        int responseCode = httpConn.getResponseCode();
-
-        // always check HTTP response code first
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            String fileName = "";
-            String disposition = httpConn.getHeaderField("Content-Disposition");
-            String contentType = httpConn.getContentType();
-            int contentLength = httpConn.getContentLength();
-
-            if (disposition != null) {
-                // extracts file name from header field
-                int index = disposition.indexOf("filename=");
-                if (index > 0) {
-                    fileName = disposition.substring(index + 10,
-                            disposition.length() - 1);
-                }
-            } else {
-                // extracts file name from URL
-                String fileURL = this.getURL_PDF().toString();
-                fileName = fileURL.substring(fileURL.lastIndexOf("/") + 1,
-                        fileURL.length());
+        public void download (String destination){
+            String urlString = "https://" + URL_PDF.toString().substring(7);
+            URL url = null;
+            System.out.println(urlString);
+            try (InputStream in = url.openStream()) {
+                url = new URL(urlString);
+                Files.copy(in, Paths.get(destination), StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-            /*
-            System.out.println("Content-Type = " + contentType);
-            System.out.println("Content-Disposition = " + disposition);
-            System.out.println("Content-Length = " + contentLength);
-            System.out.println("fileName = " + fileName);
-
-             */
-
-            // opens input stream from the HTTP connection
-            InputStream inputStream = httpConn.getInputStream();
-            String saveFilePath = saveDir + File.separator + fileName;
-
-            // opens an output stream to save into file
-            FileOutputStream outputStream = new FileOutputStream(saveFilePath);
-
-            int bytesRead = -1;
-            byte[] buffer = new byte[BUFFER_SIZE];
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                outputStream.write(buffer, 0, bytesRead);
-            }
-
-            outputStream.close();
-            inputStream.close();
-
-            System.out.println("File downloaded");
-        } else {
-            System.out.println("No file to download. Server replied HTTP code: " + responseCode);
-        }
-        httpConn.disconnect();
-    }
-
-    public void downloadIO() {
-
-        InputStream inputStream;
-        FileOutputStream fileOS;
-
-        try {
-            inputStream = getURL_PDF().openStream();
-            fileOS = new FileOutputStream("../../someArticle.pdf");
-            int i = IOUtils.copy(inputStream, fileOS);
-            System.out.println(i + " ok ");
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
-    }
+
+
 }
 
 
