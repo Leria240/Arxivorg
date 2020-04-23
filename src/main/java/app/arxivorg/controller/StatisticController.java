8837bf7b8d8f11f6bb182a39ecea44fedaa9bc8c
asus <khalilkouki14@gmail.com>
2020-04-23 17:23:24 +0200

    implémentation etaffichage des statistiques "auteurs les plus productifs"

--- src/main/java/app/arxivorg/controller/StatisticController.java
+++ src/main/java/app/arxivorg/controller/StatisticController.java
@@ -2,15 +2,17 @@ package app.arxivorg.controller;
 
 import app.arxivorg.model.Archive;
 import app.arxivorg.model.Statistic;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
+import javafx.scene.chart.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
 
@@ -20,21 +22,27 @@ public class StatisticController implements Initializable {
     @FXML public CategoryAxis categoryAxis;
     @FXML public NumberAxis numberAxis;
     @FXML public VBox statistics;
+    @FXML public Button author;
+    @FXML public Button date;
+    @FXML public ListView<String> expressionsList;
+    @FXML public Button expressions;
+    @FXML public PieChart authorsPieChart;
+    @FXML public VBox authorStatistic;
 
 
     public Archive archive;
+    private Statistic statistic = new Statistic();
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
     }
 
     public void displayCategoryBarChart() {
-        Statistic statistic = new Statistic();
         XYChart.Series<String,Integer> setOfCategories = new XYChart.Series<>();
         Map<String, Integer> categoriesData = statistic.countArticlesByCategory(archive);
-        System.out.print(categoriesData.toString());
         for (Map.Entry<String,Integer> data : categoriesData.entrySet()) {
-            setOfCategories.getData().add(new XYChart.Data<String,Integer>(data.getKey(),data.getValue()));
+            setOfCategories.getData().add(new XYChart.Data<>(data.getKey(),data.getValue()));
         }
         barChart.getData().add(setOfCategories);
         statistics.setVisible(false);
@@ -42,8 +50,22 @@ public class StatisticController implements Initializable {
     }
 
     public void displayAuthorStatistic(){
-
+        int nbAuthors_max = 15;
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+        Map<String,Integer> authorsData = statistic.mostProductiveAuthors(archive);
+        for(Map.Entry<String,Integer> data : authorsData.entrySet()){
+            pieChartData.add(new PieChart.Data(data.getKey() + " (" + data.getValue() + ")",data.getValue()));
+            nbAuthors_max --;
+            if(nbAuthors_max == 0) break;
+            System.out.println(data.getKey() + " " + data.getValue());
+        }
+        authorsPieChart.setData(pieChartData);
+        statistics.setVisible(false);
+        authorStatistic.setVisible(true);
     }
+
+
+
 
     public void displayDateStatistic(){
 
--- src/main/java/app/arxivorg/model/Statistic.java
+++ src/main/java/app/arxivorg/model/Statistic.java
@@ -16,6 +16,8 @@ public class Statistic {
         return numberOfArticlesByCategory;
     }
 
+
+
     public Map<String, Integer> mostProductiveAuthors(Archive archive){
         HashMap<String, Integer> numberOfArticlesByAuthors = new HashMap<>();
         for(Article article : archive.getSelectedArticles()){
@@ -24,14 +26,14 @@ public class Statistic {
                 numberOfArticlesByAuthors.put(author, count + 1);
             }
         }
-        numberOfArticlesByAuthors = sortedByValue(numberOfArticlesByAuthors);
-        return numberOfArticlesByAuthors;
+        return sortedByValue_descendingOrder(numberOfArticlesByAuthors);
     }
 
 
-    public static HashMap<String, Integer> sortedByValue(HashMap<String, Integer> map){
+
+    public static HashMap<String, Integer> sortedByValue_descendingOrder(HashMap<String, Integer> map){
         List<Map.Entry<String, Integer>> list = new LinkedList<>( map.entrySet() );
-        list.sort(Map.Entry.comparingByValue());
+        list.sort(Map.Entry.comparingByValue((integer, t1) -> t1-integer));
         HashMap<String, Integer> sorted_map = new LinkedHashMap<>();
         for(Map.Entry<String, Integer> entry : list)
             sorted_map.put( entry.getKey(), entry.getValue() );
